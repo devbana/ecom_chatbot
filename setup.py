b625a5ec386a336b898bc0b79193b9457bc497b7
Akash Devbanshi <akashdevbanshi14@gmail.com>
2024-08-26 17:59:24 +0530

    adding new files

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages, setup
 
-setup(name='chat_bot_',
+setup(name='chat_bot_ecomm',
       version='0.0.1',
       author='Akash Devbanshi',
       author_email='akash.devbanshi@outlook.com',
       packages=find_packages(),
-      install_requires=['langchain-astradb', 'langchain', 'langchain-openai', 'datasets', 'pypdf', 'python-dotenv', 'flask'])
+      install_requires=['langchain-astradb', 'langchain', 'langchain-openai', 'datasets', 'pypdf', 'python-dotenv', 'flask']
+      )
