cfd7b37f52f4684b4c49adc9e01bbb69be38612b
Akash Devbanshi <akashdevbanshi14@gmail.com>
2024-08-26 17:29:39 +0530

    adding initial files

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+setup(name='chat_bot_',
+      version='0.0.1',
+      author='Akash Devbanshi',
+      author_email='akash.devbanshi@outlook.com',
+      packages=find_packages(),
+      install_requires=['langchain-astradb', 'langchain', 'langchain-openai', 'datasets', 'pypdf', 'python-dotenv', 'flask'])
